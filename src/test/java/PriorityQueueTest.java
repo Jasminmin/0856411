11e8252fbb1d10d748dce3cb410967b49e537fdb
Jasminmin <42398054+Jasminmin@users.noreply.github.com>
2021-03-31 14:12:07 +0800

    ModifyToFail

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -13,11 +13,16 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class PriorityQueueTest {
     static Stream<Arguments> stringIntAndListProvider(){
         return Stream.of(
-                arguments(new int[]{3,1,2,5,4},new int[]{1,2,3,4,5}),
-                arguments(new int[]{-3,-1,-2,-4},new int[]{-4,-3,-2,-1}),
-                arguments(new int[]{3,-2,-5,-3,2,5},new int[]{-5,-3,-2,2,3,5}),
-                arguments(new int[]{-3,1,11,0,9,3},new int[]{-3,0,1,3,9,11}),
-                arguments(new int[]{3,7,2,-1,-2},new int[]{-2,-1,2,3,7})
+//                 arguments(new int[]{3,1,2,5,4},new int[]{1,2,3,4,5}),
+//                 arguments(new int[]{-3,-1,-2,-4},new int[]{-4,-3,-2,-1}),
+//                 arguments(new int[]{3,-2,-5,-3,2,5},new int[]{-5,-3,-2,2,3,5}),
+//                 arguments(new int[]{-3,1,11,0,9,3},new int[]{-3,0,1,3,9,11}),
+//                 arguments(new int[]{3,7,2,-1,-2},new int[]{-2,-1,2,3,7})
+                arguments(new int[]{},new int[]{}),
+                arguments(new int[]{},new int[]{}),
+                arguments(new int[]{},new int[]{}),
+                arguments(new int[]{},new int[]{}),
+                arguments(new int[]{},new int[]{-2,-1,2,3,7})
         );
     }
     @ParameterizedTest(name="#{index} - Test with Argument={0},{1}")
